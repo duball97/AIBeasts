1dd404525e8386bbc3ded22fb9ab7e49e5cd9382
Vortex Dapp <megabobbyinu@gmail.com>
2025-02-07 01:03:37 +0000

    Update openaiClient.js

--- openaiClient.js
+++ openaiClient.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.VITE_OPENAI_API_KEY, // Use process.env for backend environment variables
+  apiKey: process.env.VITE_OPENAI_API_KE Y, // Use process.env for backend environment variables
 });
 
 export default openai;
