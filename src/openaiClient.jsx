18d34e40b1d844b2962c6ddf9a9e3cbfd6d661fc
Vortex Dapp <megabobbyinu@gmail.com>
2025-02-07 01:05:01 +0000

    A

--- src/openaiClient.jsx
+++ src/openaiClient.jsx
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY, // Add this key to your .env file
+  apiKey: import.meta.VITE_OPENAI_API_KEY, // Add this key to your .env file
 });
 
 export default openai;
