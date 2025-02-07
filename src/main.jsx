d12357d0da554b1247cb41c33b6d2851275a1fe6
Vortex Dapp <megabobbyinu@gmail.com>
2025-02-07 01:04:15 +0000

    a

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css'; // Import the global styles
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React. StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
